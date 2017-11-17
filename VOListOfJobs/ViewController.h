892c30feb5e831c0214310bdb4e240c6550d520f
orlov <orlov@iMac.local>
2017-11-17 15:46:47 +0300

    Initial Commit

---
+++ VOListOfJobs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VOListOfJobs
+//
+//  Created by iMac on 17.11.2017.
+//  Copyright © 2017 VasiliOrlovCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
